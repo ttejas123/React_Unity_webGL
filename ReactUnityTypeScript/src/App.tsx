d36b0383ef919ba7ef4b805570c960608e5b460b
Tejas Thakare <tthakare73@gmail.com>
2023-04-09 17:42:03 +0000

    Uinity Game Project

---
+++ ReactUnityTypeScript/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import UnityContainer from './Unity/UnityContainer'
+
+function App() {
+  return (
+    <div style={{width: "100%", height: "100vh"}} >
+      <UnityContainer />
+    </div>
+  )
+}
+
+export default App
---
+++ ReactUnityTypeScript/src/Unity/UnityContainer.tsx
@@ -0,0 +1,25 @@
+import { Unity, useUnityContext } from "react-unity-webgl";
+
+function UnityContainer() {
+  const { unityProvider, isLoaded, loadingProgression, addEventListener, removeEventListener, sendMessage }= useUnityContext({
+    loaderUrl: "./Build/Zoom.loader.js",
+    dataUrl: "./Build/Zoom.data.br",
+    frameworkUrl: "./Build/Zoom.framework.js.br",
+    codeUrl: "./Build/Zoom.wasm.br",
+  });
+  return (
+    <div className="UnityContainer">
+      <Unity
+            style={{
+                width: "100%",
+                height: "100vh",
+                justifyContent:"center",
+                alignItems: "center"
+            }}
+          className="unity-canvas" devicePixelRatio={3}  unityProvider={unityProvider}   
+        />
+    </div>
+  )
+}
+
+export default UnityContainer
