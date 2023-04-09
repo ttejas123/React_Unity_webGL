5147df17c434424df84c8ed74947b7d0ff05f6ce
Tejas Thakare <tthakare73@gmail.com>
2023-04-09 18:07:41 +0000

    Changes

--- ReactUnityTypeScript/src/Unity/UnityContainer.tsx
+++ ReactUnityTypeScript/src/Unity/UnityContainer.tsx
@@ -2,10 +2,10 @@ import { Unity, useUnityContext } from "react-unity-webgl";
 
 function UnityContainer() {
   const { unityProvider, isLoaded, loadingProgression, addEventListener, removeEventListener, sendMessage }= useUnityContext({
-    loaderUrl: "./Build/Zoom.loader.js",
-    dataUrl: "./Build/Zoom.data.br",
-    frameworkUrl: "./Build/Zoom.framework.js.br",
-    codeUrl: "./Build/Zoom.wasm.br",
+    loaderUrl: "Build/Zoom.loader.js",
+    dataUrl: "Build/Zoom.data.br",
+    frameworkUrl: "Build/Zoom.framework.js.br",
+    codeUrl: "Build/Zoom.wasm.br",
   });
   return (
     <div className="UnityContainer">
